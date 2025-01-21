748ac91744733e21d4c650f56a68d5488be73d3f
zaarrs <khairul0104@student.abudzar.sch.id>
2025-01-21 13:41:55 +0700

    Create tugas.9.js

---
+++ M10/tugas.9.js
@@ -0,0 +1,28 @@
+// soal1
+let nilaiMTK = 91;
+let nilaiIPA = 81;
+let nilaiIPS = 91;
+
+if(nilaiMTK >= 91 && nilaiIPA >= 91 && nilaiIPS >= 91){
+    console.log('lulus')
+}else{
+    console.log('tidak lulus')
+}
+//soal2
+  let nilaiMtk = 91;
+  let nilaiIpa = 81;
+  let nilaiIps = 91;
+
+  if(nilaiMtk >= 91 || nilaiIpa >= 91 || nilaiIps >= 91){
+    console.log('andi lulus')
+  }else{
+    console.log('tidak lulus')
+  }
+  //soal3
+  let isLogin = false;
+  if(!isLogin){
+    console.log('anda belum login')
+
+  }else{
+    console.log('anda sudah login')
+  }
