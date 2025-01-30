484c42eb3ab6210980886df078b10be561e87fbe
zaarrs <khairul0104@student.abudzar.sch.id>
2025-01-30 12:02:28 +0700

    tugas 11

---
+++ M13/index.js
@@ -0,0 +1,58 @@
+//soal 1
+let angka = 1;
+
+while(angka <= 10) {
+    console.log(angka);
+    angka++;
+}
+
+//soal 2
+let angka2 = 10;
+let hasil = 0;
+while(angka2 >= 1)
+{
+    hasil += angka2;
+    angka2--;
+    console.log (angka2);
+}
+
+//soal 3
+let angka3 = 1;
+let hasil3 = 0;
+while(angka3 <= 10)
+{
+    hasil3 += angka3;
+    angka3++;
+    console.log(angka3);
+}
+
+//soal 4
+let angka4 = 1;
+let hasil4 = 0;
+let i = 0;
+while(angka4 <= 10)
+{
+    if(i % 2 == 0)
+    {
+        hasil4 += angka4;
+        {
+            console.log(angka4);
+            {
+                angka4++;
+            }
+        }
+    }
+}
+//soal 5
+let angka5 = 1;
+let hasil5 = 0;
+let i = 0;
+while(angka5 <= 10)
+{
+    if(i % 2 == 0)
+}
+
+
+
+    
+
