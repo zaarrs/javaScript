f542e8042a3a180d8168060fe085aad2c354b787
zaarrs <khairul0104@student.abudzar.sch.id>
2025-01-30 13:24:58 +0700

    TUGAS 12

---
+++ M14/tugas12.js
@@ -0,0 +1,55 @@
+// Tugas 1
+function hello() {
+    console.log("Hello, World!");
+  }
+  hello();
+  
+  // Tugas 2
+  function pesan() {
+    let message = "ini merupakan pesan lokal";
+    console.log(message);
+  }
+  pesan();
+  
+  // Tugas 3
+  let userName = "guest";
+  
+  function updateUserName() {
+    userName = "admin";
+    console.log(userName);
+  }
+  updateUserName();
+  
+  // Tugas 4
+  ("TIDAKADA");
+  
+  // Tugas 5
+  function greet(name = "user") {
+    console.log(`Hello,${name}`);
+  }
+  greet();
+  
+  // Tugas 6
+  function setUser(Guest) {
+    Guest = Guest || "nizar";
+    console.log(`Welcome, ${Guest}`);
+  }
+  setUser();
+  
+  // Tugas 7
+  function add(a, b) {
+    return a + b;
+  }
+  console.log(add(10, 7));
+
+
+  //#BEZIRUTAL
+  
+
+
+
+
+
+
+
+
